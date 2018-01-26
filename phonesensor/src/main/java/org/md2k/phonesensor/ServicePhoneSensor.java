46ad4e2ccd34b62b8e1f9b678cd5789d114e248b
Xavier Tilley <hippietilley@gmail.com>
2018-01-26 11:50:24 -0600

    MC-757 Documented ServicePhoneSensor

--- phonesensor/src/main/java/org/md2k/phonesensor/ServicePhoneSensor.java
+++ phonesensor/src/main/java/org/md2k/phonesensor/ServicePhoneSensor.java
@@ -51,16 +51,27 @@ import org.md2k.phonesensor.phone.sensors.PhoneSensorDataSources;
 import br.com.goncalves.pugnotification.notification.PugNotification;
 import es.dmoral.toasty.Toasty;
 
+/**
+ *
+ */
 public class ServicePhoneSensor extends Service {
     public static final String INTENT_STOP = "intent_stop";
     private static final String TAG = ServicePhoneSensor.class.getSimpleName();
     PhoneSensorDataSources phoneSensorDataSources = null;
     DataKitAPI dataKitAPI;
 
+    /**
+     * When this service is created it checks for permission information
+     */
     public void onCreate() {
         super.onCreate();
         PermissionInfo permissionInfo = new PermissionInfo();
         permissionInfo.getPermissions(this, new ResultCallback<Boolean>() {
+            /**
+             * Shows notification when permissions are denied.
+             *
+             * @param result Whether appropriate permissions are given or not
+             */
             @Override
             public void onResult(Boolean result) {
                 if (!result) {
@@ -74,19 +85,32 @@ public class ServicePhoneSensor extends Service {
             }
         });
     }
+
+    /**
+     * Shows a notification to ask for permission
+     */
     private void showNotification() {
         Bundle bundle = new Bundle();
         bundle.putInt(ActivityMain.OPERATION, ActivityMain.OPERATION_START_BACKGROUND);
         PugNotification.with(this).load().identifier(13).title("Permission required").smallIcon(R.mipmap.ic_launcher)
-                .message("PhoneSensor app can't continue. (Please click to grant permission)").autoCancel(true).click(ActivityMain.class, bundle).simple().build();
+                .message("PhoneSensor app can't continue. (Please click to grant permission)")
+                .autoCancel(true).click(ActivityMain.class, bundle).simple().build();
     }
+
+    /**
+     * Removes a notification
+     */
     private void removeNotification() {
         PugNotification.with(this).cancel(13);
     }
 
+    /**
+     * Loads the service, reads the settings and connects to <code>dataKitAPI</code>.
+     */
     void load() {
         LogStorage.startLogFileStorageProcess(getApplicationContext().getPackageName());
-        Log.w(TAG, "time=" + DateTime.convertTimeStampToDateTime(DateTime.getDateTime()) + ",timestamp=" + DateTime.getDateTime() + ",service_start");
+        Log.w(TAG, "time=" + DateTime.convertTimeStampToDateTime(DateTime.getDateTime()) + ",timestamp="
+                + DateTime.getDateTime() + ",service_start");
         LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver, new IntentFilter(INTENT_STOP));
         if (!readSettings()) {
             Toasty.error(this, "Error: not configured yet", Toast.LENGTH_SHORT).show();
@@ -94,10 +118,21 @@ public class ServicePhoneSensor extends Service {
         } else connectDataKit();
     }
 
+    /**
+     * Connects <code>phonesensor</code> to <code>dataKitAPI</code> by getting the application context
+     * of <code>dataKitAPI</code> and passing it a new <code>OnConnectionListener</code> object.
+     *
+     * If the connection fails, it is logged, Toasty produces an error message,
+     * <code>disconnectDataKit</code> is called and the service is stopped.
+     */
     private void connectDataKit() {
         dataKitAPI = DataKitAPI.getInstance(getApplicationContext());
         try {
             dataKitAPI.connect(new OnConnectionListener() {
+                /**
+                 * When <code>dataKitAPI</code> has been connected successfully,
+                 * <code>phoneSensorDataSources.register</code> is called to register the data sources.
+                 */
                 @Override
                 public void onConnected() {
                     try {
@@ -115,6 +150,10 @@ public class ServicePhoneSensor extends Service {
         }
     }
 
+    /**
+     * Unregisters Android's message receiver from <code>LocalBroadcastManager</code>,
+     * unregisters any remaining phone sensors and runs <code>dataKitAPI.disconnect</code>.
+     */
     private synchronized void disconnectDataKit() {
         LocalBroadcastManager.getInstance(this).unregisterReceiver(
                 mMessageReceiver);
@@ -127,27 +166,49 @@ public class ServicePhoneSensor extends Service {
         }
     }
 
+    /**
+     * Gets the application context of <code>PhoneSensorDataSources</code> and then counts the number
+     * of data sources
+     *
+     * @return True when <code>phoneSensorDataSources.countEnabled</code> is not 0.
+     */
     private boolean readSettings() {
         phoneSensorDataSources = new PhoneSensorDataSources(getApplicationContext());
         return phoneSensorDataSources.countEnabled() != 0;
     }
 
+    /**
+     * Logs the timestamp of <code>service_stop</code>, disconnects dataKit, and calls super.
+     */
     @Override
     public void onDestroy() {
-        Log.w(TAG, "time=" + DateTime.convertTimeStampToDateTime(DateTime.getDateTime()) + ",timestamp=" + DateTime.getDateTime() + ",service_stop");
+        Log.w(TAG, "time=" + DateTime.convertTimeStampToDateTime(DateTime.getDateTime()) + ",timestamp="
+                + DateTime.getDateTime() + ",service_stop");
         LocalBroadcastManager.getInstance(this).unregisterReceiver(mMessageReceiver);
         disconnectDataKit();
         super.onDestroy();
     }
 
+    /**
+     * This method has not been implemented yet.
+     *
+     * @param intent Android intent
+     * @return
+     */
     @Override
     public IBinder onBind(Intent intent) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
+    /**
+     * Creates a new broadcast receiver that receives the <code>stop_service</code> intent. Upon
+     * receipt it disconnects dataKit and stops itself.
+     */
     private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Log.w(TAG, "time=" + DateTime.convertTimeStampToDateTime(DateTime.getDateTime()) + ",timestamp=" + DateTime.getDateTime() + ",broadcast_receiver_stop_service");
+            Log.w(TAG, "time=" + DateTime.convertTimeStampToDateTime(DateTime.getDateTime()) + ",timestamp="
+                    + DateTime.getDateTime() + ",broadcast_receiver_stop_service");
             disconnectDataKit();
             stopSelf();
         }
